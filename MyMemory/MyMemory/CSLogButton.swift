3f3f9c536f8f843b5f0febb6bf271c15f5d1c9d5
9yubean2 <mi701170@naver.com>
2022-05-07 01:14:47 +0900

    MyMemory add Custom Button & Custom Alert View

---
+++ MyMemory/MyMemory/CSLogButton.swift
@@ -0,0 +1,43 @@
+//
+//  CSLogButton.swift
+//  MyMemory
+//
+//  Created by 이규빈 on 2022/05/07.
+//
+
+import UIKit
+
+public enum CSLogType: Int {
+    case basic //기본 로그 타입
+    case title //버튼의 타이틀을 출력
+    case tag //버튼의 태그값 출력
+}
+
+class CSLogButton: UIButton {
+    
+    //로그 출력 타입
+    public var logType: CSLogType = .basic
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //버튼에 스타일 적용
+        self.setBackgroundImage(UIImage(named: "button-bg"), for: .normal)
+        self.tintColor = .white
+        
+        //클릭 이벤트에 logging 메소드 적용
+        self.addTarget(self, action: #selector(logging(_:)), for: .touchUpInside)
+    }
+    
+    //로그 출력 메소드
+    @objc func logging(_ sender: UIButton) {
+        switch self.logType {
+        case .basic: //기본 로그 출력
+            NSLog("Button is Selected")
+        case .title:
+            let btnTitle = sender.titleLabel?.text ?? "Untitled"
+            NSLog("\(btnTitle)Button is Selected")
+        case .tag:
+            NSLog("\(sender.tag)Button is Selected")
+        }
+    }
+}
--- MyMemory/MyMemory/MemoFormVC.swift
+++ MyMemory/MyMemory/MemoFormVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
 
     var subject: String!
+    
     //MARK: - Properties
     
     @IBOutlet weak var contentTV: UITextView!
@@ -17,10 +18,16 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
     
     //save Button
     @IBAction func saveBtn(_ sender: Any) {
+        //alert contents view
+        let alertView = UIViewController()
+        let iconImage = UIImage(named: "warning-icon-60")
+        alertView.view = UIImageView(image: iconImage)
+        alertView.preferredContentSize = iconImage?.size ?? CGSize.zero
         //non data
         guard self.contentTV.text?.isEmpty == false else {
             let alert = UIAlertController(title: nil, message: "Enter Contents", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.setValue(alertView, forKey: "contentViewController")
             self.present(alert, animated: true)
             return
         }
@@ -57,17 +64,44 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
         picker.dismiss(animated: false)
 
     }
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
-//        super.viewDidLoad()
+        
+
         self.contentTV.delegate = self
         
+        //set background
+        let bgImage = UIImage(named:"memo-background")!
+        self.view.backgroundColor = UIColor(patternImage: bgImage)
+        
+        //textview basic property
+        self.contentTV.layer.borderWidth = 0
+        self.contentTV.layer.borderColor = UIColor.clear.cgColor
+        self.contentTV.backgroundColor = UIColor.clear
+        
+        //line spacing
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 8
+        self.contentTV.attributedText = NSAttributedString(string: " ", attributes: [.paragraphStyle: style])
+        self.contentTV.text = ""
     }
+    
+    // MARK: - Helper
     func textViewDidChange(_ textView: UITextView) {
         let contentTV = textView.text as NSString
         let length = ((contentTV.length > 15) ? 15 : contentTV.length)
         self.subject = contentTV.substring(with: NSRange(location: 0, length: length))
         
         self.navigationItem.title = self.subject
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let bar = self.navigationController?.navigationBar
+        
+        let ts = TimeInterval(0.3)
+        UIView.animate(withDuration: ts) {
+            bar?.alpha = ( bar?.alpha == 0 ? 1 : 0 )
+        }
     }
 }
